9d9b924cfbaea548450a7c978854d105a484caaa
Mogarampalli Niharika <mniharika26@gmail.com>
2021-05-28 13:34:43 +0530

    second commit

--- app/src/main/java/com/example/archaezooapp/register.java
+++ app/src/main/java/com/example/archaezooapp/register.java
@@ -3,6 +3,8 @@ package com.example.archaezooapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void register(View view) {
+        Toast.makeText(this, "Registered successfully", Toast.LENGTH_SHORT).show();
     }
 }
